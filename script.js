319ae13ea7b39d4f652d99e951f906be190e347f
Shreya Yadav <267423381+codesbyshreya@users.noreply.github.com>
2026-07-05 20:57:35 +0530

    Upgarded website

--- script.js
+++ script.js
@@ -1,41 +1,58 @@
 let addbtn=document.querySelector("#buttonadd");
 let subbtn=document.querySelector("#buttonsub");
-let resetbtn=document.querySelector("button");
+let resetbtn=document.querySelector("#reset");
 let count=document.querySelector("#total-count")
-let warning=document.querySelector("#message")
+let togglebtn=document.querySelector("#toggle");
+
+let isDark=true;
+
+togglebtn.addEventListener("click", function toggle(){
+    main = document.querySelector(".main");
+    if(isDark){
+        main.style.backgroundColor = "white";
+        main.style.color = "black";
+        isDark = false;
+        togglebtn.textContent = "Dark Mode";
+    } else {
+        main.style.backgroundColor = "black";
+        main.style.color = "white";
+        isDark = true;
+        togglebtn.textContent = "Light Mode";
+    }
+});
 
 let value=0;
 
 addbtn.addEventListener("click", function add(){
-     if(value >= 10){
-        warning.style.display = "block";
-        return;
-    }else{
-        warning.style.display = "none";
-    }
+    //  if(value >= 10){
+    //     warning.style.display = "block";
+    //     return;
+    // }else{
+    //     warning.style.display = "none";
+    // }
     value += 1;
     count.textContent = value;
-    count.style.boxShadow = "0px 0px 100px green";
+    count.style.boxShadow = "0px 0px 100px #1df10596";
     updateColor();
 })
 
 subbtn.addEventListener("click", function sub(){
-   if(value <= -10){
-        warning.style.display = "block";
-        return;
-    }else{
-        warning.style.display = "none";
-    }
+//    if(value <= -10){
+//         warning.style.display = "block";
+//         return;
+//     }else{
+//         warning.style.display = "none";
+//     }
     value -= 1;
     count.textContent = value;
-    count.style.boxShadow = "0px 0px 100px red";
+    count.style.boxShadow = "0px 0px 100px #f1340596";
     updateColor();
 })
 
 resetbtn.addEventListener("click", function reset(){
     value = 0;
     count.textContent = value;
-    count.style.boxShadow = "0px 0px 100px yellow";
+    count.style.boxShadow = "0px 0px 100px #68c3bb96";
     updateColor();
 })
 
